10aca61aa17c92931ce02f5fb03e84cd0488d5a8
nhat2012782 <gonbbb1@gmail.com>
2022-01-02 22:25:16 +0700

    commit on main

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+
+
+toast({
+    title: 'Success123',
+    message: 'Tham gia nhóm Học lập trình tại F8 trên Facebook để cùng nhau trao đổi trong quá trình học tập 12345',
+    type: 'Info',
+    duration: 3000,
+});
